5724ad7900675495d5685125bde92eea03c81da5
ltsoveranakin <109498476+ltsoveranakin@users.noreply.github.com>
2026-09-22 01:53:28 -0400

    Add plus icons around the heading

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,35 @@
 import styles from "./Header.module.scss";
 import corner_tri from "../assets/img/corner_tri.svg";
+import plus_icon from "../assets/img/plus_icon.svg";
 import ConsoleCursor from "./ConsoleCursor.tsx";
 
 const IMAGE_SIZE = 100;
+const PLUS_ICON_SIZE = 25;
+const stylizedGraphicText = "Stylized graphic";
 
 export default function Header() {
 	return (
 		<div className={styles.header}>
 			<img
 				src={corner_tri}
-				alt={"Corner stylized image"}
+				alt={stylizedGraphicText}
 				width={IMAGE_SIZE}
 				className={styles.cornerTri}
 			/>
 
-			<div className={styles.titleContainer}>
-				<h1 className={styles.title}>
-					Takoda_Paschel // Programmer // Game_Dev
-				</h1>
-				<ConsoleCursor color={"#c0fe04"} rem={3} />
+			<div className={styles.titleBoxContainer}>
+				<div className={styles.titleBox}>
+					<PlusColumn />
+
+					<div className={styles.titleContainer}>
+						<h1 className={styles.title}>
+							Takoda_Paschel // Programmer // Game_Dev
+						</h1>
+						<ConsoleCursor color={"#c0fe04"} rem={3} />
+					</div>
+
+					<PlusColumn />
+				</div>
 			</div>
 
 			<div
@@ -28,6 +39,24 @@ export default function Header() {
 					flexShrink: 0,
 				}}
 			></div>
+		</div>
+	);
+}
+
+function PlusColumn() {
+	return (
+		<div className={styles.plusColumn}>
+			<img
+				src={plus_icon}
+				alt={stylizedGraphicText}
+				width={PLUS_ICON_SIZE}
+			/>
+			<div className={styles.plusRowSpacer} />
+			<img
+				src={plus_icon}
+				alt={stylizedGraphicText}
+				width={PLUS_ICON_SIZE}
+			/>
 		</div>
 	);
 }
