0b2471512657f413ab1bbd27843141e6b19d2c5b
ltsoveranakin <109498476+ltsoveranakin@users.noreply.github.com>
2026-09-25 23:36:11 -0400

    Create static overlay files

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.scss";
 import Contact from "./components/Contact.tsx";
 import Projects from "./components/projects/Projects.tsx";
 import AboutMe from "./components/AboutMe.tsx";
+import StaticOverlay from "./components/StaticOverlay.tsx";
 
 export default function App() {
 	return (
@@ -11,6 +12,7 @@ export default function App() {
 			<AboutMe />
 			<Projects />
 			<Contact />
+			<StaticOverlay />
 		</>
 	);
 }
